c29178d790debfa9249d8ccb7c5460b202e1f9bf
MWHunter <s0521458@student.rockvalleycollege.edu>
2023-01-11 11:07:45 -0600

    Use worldedit api

---
+++ README.md
@@ -0,0 +1,7 @@
+A block replacer for WorldEdit.
+
+Allows you to replace all oak stairs, slabs, logs, etc. to any type of stone or a different type of wood with one command
+
+Product of a failed commission request due to JustMistyMC not willing to pay 15% upfront before I send the plugin.
+
+License is GPLv3 with an exception that you don't compile the plugin and give it to anyone called JustMistyMC.
--- src/main/java/io/defineoutside/blockreplacer/BlockReplacer.java
+++ src/main/java/io/defineoutside/blockreplacer/BlockReplacer.java
@@ -1,6 +1,14 @@
 package io.defineoutside.blockreplacer;
 
-import org.bukkit.Bukkit;
+import com.sk89q.worldedit.*;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.bukkit.BukkitPlayer;
+import com.sk89q.worldedit.function.mask.BlockTypeMask;
+import com.sk89q.worldedit.function.pattern.TypeApplyingPattern;
+import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.session.SessionManager;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldedit.world.block.BlockState;
 import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.command.Command;
@@ -15,22 +23,7 @@ import java.util.Locale;
 
 public final class BlockReplacer extends JavaPlugin implements CommandExecutor {
 
-    List<Tag<Material>> blockTags = Arrays.asList(
-            Tag.ALL_SIGNS,
-            Tag.BASE_STONE_NETHER,
-            Tag.BASE_STONE_OVERWORLD,
-            Tag.BUTTONS,
-            Tag.DOORS,
-            Tag.FENCES,
-            Tag.FENCE_GATES,
-            Tag.LOGS,
-            Tag.PLANKS,
-            Tag.PRESSURE_PLATES,
-            Tag.SLABS,
-            Tag.STAIRS,
-            Tag.TRAPDOORS,
-            Tag.WALLS
-    );
+    List<Tag<Material>> blockTags = Arrays.asList(Tag.ALL_SIGNS, Tag.BASE_STONE_NETHER, Tag.BASE_STONE_OVERWORLD, Tag.BUTTONS, Tag.DOORS, Tag.FENCES, Tag.FENCE_GATES, Tag.LOGS, Tag.PLANKS, Tag.PRESSURE_PLATES, Tag.SLABS, Tag.STAIRS, Tag.TRAPDOORS, Tag.WALLS);
 
     @Override
     public void onEnable() {
@@ -48,8 +41,26 @@ public final class BlockReplacer extends JavaPlugin implements CommandExecutor {
             return false;
         }
 
+        BukkitPlayer actor = BukkitAdapter.adapt(player);
+        SessionManager manager = WorldEdit.getInstance().getSessionManager();
+        LocalSession session = manager.get(actor);
+
+        Region region; // declare the region variable
+        World selectionWorld = session.getSelectionWorld();
+        try {
+            if (selectionWorld == null) throw new IncompleteRegionException();
+            region = session.getSelection(selectionWorld);
+        } catch (IncompleteRegionException ex) {
+            sender.sendMessage("Please make a selection first");
+            return false;
+        }
+
+        EditSession editSession = WorldEdit.getInstance().newEditSession(selectionWorld);
+
         String argOne = args[0].toUpperCase(Locale.ROOT);
         String argTwo = args[1].toUpperCase(Locale.ROOT);
+
+        int replaced = 0;
 
         for (Tag<Material> tag : blockTags) {
             Material typeOne = null;
@@ -68,10 +79,25 @@ public final class BlockReplacer extends JavaPlugin implements CommandExecutor {
                 }
             }
 
+
             if (typeOne != null && typeTwo != null) {
-                player.performCommand("/replace " + typeOne.name() + " ^" + typeTwo.name());
+                BlockTypeMask typeMask = new BlockTypeMask(selectionWorld, BukkitAdapter.asBlockType(typeOne));
+
+                BlockState twoType = BukkitAdapter.asBlockType(typeTwo).getDefaultState();
+                TypeApplyingPattern pattern = new TypeApplyingPattern(selectionWorld, twoType);
+
+                try {
+                    replaced += editSession.replaceBlocks(region, typeMask, pattern);
+                } catch (MaxChangedBlocksException e) {
+                    return true;
+                }
             }
         }
+
+        sender.sendMessage("Replaced " + replaced + " blocks");
+
+        editSession.close();
+        session.remember(editSession);
 
         return true;
     }
